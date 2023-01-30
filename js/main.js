428b760b1867c8a84b18f58cc3668f310b6977f8
dann <danil.mekh@mail.ru>
2023-01-30 17:52:10 +0700

    Вычисляемые свойства
    (8)

--- js/main.js
+++ js/main.js
@@ -2,44 +2,61 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
-        inventory: 100,
-        onSale: false,
+        inStock: true,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
 
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
     },
+
     methods: {
         addToCart() {
             this.cart += 1
         },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
 
-        removeCart() {
-            this.cart -= 1
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
         },
 
-        updateProduct(variantImage) {
-            this.image = variantImage
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return  this.brand + ' ' + this.product + ' are not on sale'
         }
 
     }
 
 })
-
-
